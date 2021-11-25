6f35161463e427ae9d2ce026c1b00f4ebb79fb63
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-11-25 18:07:17 -0500

    Create r_history.sh

---
+++ bash/r_history.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+searchWord="$1"
+
+if [[ $# -gt 1 ]]
+then
+    size="$2"
+else
+    size=20
+fi
+
+OLDIFS=$IFS
+
+IFS='
+'
+
+output=$(cat $HISTFILE | /bin/grep "$searchWord" | /bin/grep -v "rh " | uniq | tail -n $size)
+
+outputArray=($output)
+
+IFS=$OLDIFS
+
+# Print all possible commands
+for ((i=0;i<${#outputArray[@]};i++))
+do
+    echo -e "$i:\t${outputArray[$i]}"
+done
+
+if [[ ${#outputArray[@]} -gt 0 ]]
+then
+    read input
+
+    isNumber=$(echo "$input" | /bin/egrep '^[0-9]+$')
+
+    
+    if [[ -z "$isNumber" || "$input" -ge $size ]]
+    then
+        echo "input is not valid."
+    else
+        command=${outputArray[$input]}
+        # custom append the executed command to the history
+        history -s "$command"
+        # execute command
+        eval $command
+    fi
+fi
+
